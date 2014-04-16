55c50272626c138725244f61a7fd0986e869aa15
Kevin Sprong <kevinsprong@gmail.com>
2014-04-15 21:16:53 -0400

    analysis script

---
+++ Driver2048/src/main/java/com/kevinsprong/gamesolver/PlyAnalysis.java
@@ -0,0 +1,153 @@
+package com.kevinsprong.gamesolver;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Class to play one (or many) game(s) of 2048 using the TwoZeroFourEight class
+ */
+public class PlyAnalysis {
+	public static void main( String[] args ) throws IOException {
+		// num sims per parameter setting
+		int numTrials = 20;
+
+		// sim parameters
+		double [] plyRange = {1, 10, 1};
+
+		ArrayList<Double> plyVec = new ArrayList<Double>();
+
+		for (double pR = plyRange[0]; pR <= plyRange[1]; pR += plyRange[2]) {
+			plyVec.add(pR);
+		}
+
+		int totalNumSettings = plyVec.size();
+
+		// variables to hold game info
+		TwoZeroFourEight game;
+		int winStatus;
+		int highTile;
+		double score;
+
+		// variables to hold sim results
+		int highestTile = 0;
+		int[] winRecord = new int[numTrials];
+		int[] highTiles = new int[numTrials];
+		int[] scores  = new int[numTrials];
+		double avgWinPct;
+		double avgHighTile;
+		double avgScore;
+
+		// file to write to
+		BufferedWriter writer = null;
+		String resultsFilePath = "PlyAnalysis2048.csv";
+		File resultsFile = new File(resultsFilePath);
+		String newline = System.getProperty("line.separator");
+		System.out.println(resultsFile.getCanonicalPath());
+		writer = new BufferedWriter(new FileWriter(resultsFile));
+		writer.write("ply,avgWinPct,avgHighTile,highTile,avgScore" +
+				newline);
+
+		// loop to optimize parameters
+		long startTime = System.nanoTime();
+		int thisSetting = 0;
+		for (double pR : plyVec) {
+
+			thisSetting++;
+
+
+
+			highestTile = 0;
+			winRecord = new int[numTrials];
+			highTiles = new int[numTrials];
+			scores  = new int[numTrials];
+
+			for (int k = 0; k < numTrials; k++) {
+
+				System.out.println(Double.toString(pR) + ", " +
+					" Setting " + thisSetting + 
+					" of " + totalNumSettings + 
+					", Trial " + Integer.toString(k+1));
+
+				// create a new game
+				game = new TwoZeroFourEight("AlphaBeta", "DefaultComputer");
+				game.setSearchPly(7);
+				game.setWinCondition(65536);
+				game.setHeuristicWeights(new double[]{wR, mR, sR, oR});
+				game.initializeBoard();
+
+				// play the game until there is a winner
+				winStatus = game.determineWinner();
+				while (winStatus == 0) {
+					// move player who is currently to move;
+					game.playerMove();
+					game.updateGameState();	
+					winStatus = game.determineWinner();		
+				}
+
+				// get game results
+				score = game.getGameState().getGameScore();
+				highTile = getHighTile(game.getGameState().getBoardState());
+
+				// update results vectors
+				if (highTile > highestTile) {
+					highestTile = highTile;
+				}
+				scores[k] = (int) score;
+				highTiles[k] = highTile;
+				if (highTile >= 2048) {
+					winRecord[k] = 1;
+				} else {
+					winRecord[k] = 0;
+				}
+			}
+
+			// summarize trials and write to file
+			avgWinPct = calculateAverage(winRecord);
+			avgHighTile = calculateAverage(highTiles);
+			avgScore = calculateAverage(scores);
+
+			writer.write(
+					Double.toString(pR) + "," +
+						Double.toString(avgWinPct) + "," +
+						Double.toString(avgHighTile) + "," +
+						Integer.toString(highestTile) + "," +
+						Double.toString(avgScore) + newline
+					);
+		}
+		double elapsedTime = (double) (System.nanoTime() - startTime) / 1e9;
+		System.out.println("Done!  Time Elapsed: " + 
+				Double.toString(elapsedTime) + " s");
+
+		// close our writer
+		writer.close();
+
+	}
+
+    public static int getHighTile(int[][] board) {
+    	int maxTile = 0;
+    	for (int i = 0; i < 4; i++) {
+    		for (int j = 0; j < 4; j++) {
+    			if (board[i][j] > maxTile) {
+    				maxTile = board[i][j];
+    			}
+    		}
+    	}
+    	return maxTile;
+    }
+
+    public static double calculateAverage(int[] marks) {
+    	double sum = 0;
+    	if(marks.length > 0) {
+    		for (int mark : marks) {
+    			sum += mark;
+    		}
+    		return (double) sum / marks.length;
+    	}
+    	return sum;
+    }
+    
+
+}
